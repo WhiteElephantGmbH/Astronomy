8b1e2ffd1a268546c631dcbdb70b2a185d1236a9
Urs Maurer <Urs@Maurer.net>
2021-05-04 17:24:51 +0200

    Handle task exceptions

--- Ada/Projects/Sky/Network-Tcp-Server.adb
+++ Ada/Projects/Sky/Network-Tcp-Server.adb
@@ -1,5 +1,5 @@
 -- *********************************************************************************************************************
--- *                       (c) 2017 .. 2018 by White Elephant GmbH, Schaffhausen, Switzerland                          *
+-- *                       (c) 2017 .. 2021 by White Elephant GmbH, Schaffhausen, Switzerland                          *
 -- *                                               www.white-elephant.ch                                               *
 -- *                                                                                                                   *
 -- *    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General     *
@@ -175,6 +175,9 @@ package body Network.Tcp.Server is
     end loop Main;
     accept Finish;
     Log_Write ("finished");
+  exception
+  when Item: others =>
+    Log.Termination (Item);
   end Listener;
 
 
