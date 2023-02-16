c0aeaef2fac6a73f79173ac27baec2faffb27374
Urs Maurer <Urs@Maurer.net>
2023-02-16 18:12:50 +0100

    ENC_2302 - v1.0/0-1 - Added Icon.

--- Ada/Projects/Sky/ENC_2302/ENC_2302.adb
+++ Ada/Projects/Sky/ENC_2302/ENC_2302.adb
@@ -1,13 +1,12 @@
 -- *********************************************************************************************************************
--- *                           (c) 2022 by White Elephant GmbH, Schaffhausen, Switzerland                              *
+-- *                       (c) 2022 .. 2023 by White Elephant GmbH, Schaffhausen, Switzerland                          *
 -- *                                               www.white-elephant.ch                                               *
 -- *********************************************************************************************************************
 pragma Style_White_Elephant;
 
 pragma Build (Description => "ENC 2302 Simulator",
-              Version     => (1, 0, 0, 0),
+              Version     => (1, 0, 0, 1),
               Kind        => Console,
-              Icon        => False,
               Libraries   => ("AWS64", "COLL64"),
               Compiler    => "GNATPRO\23.0");
 with Simulator;
