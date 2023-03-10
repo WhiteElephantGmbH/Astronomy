a4b781d0f6170e80a3b341c31272e3e4e71638a8
Urs Maurer <Urs@Maurer.net>
2023-03-10 12:06:31 +0100

    Added project TimeServer

---
+++ Modula/Projects/TimeServer/Main.ads
@@ -0,0 +1,11 @@
+-- *********************************************************************************************************************
+-- *                           (c) 2023 by White Elephant GmbH, Schaffhausen, Switzerland                              *
+-- *                                               www.white-elephant.ch                                               *
+-- *********************************************************************************************************************
+pragma Style_White_Elephant;
+
+package Main is
+
+  procedure Initialize;
+
+end Main;
