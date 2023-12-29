fdd2255d0254fc9b8456dd5eac6ce0121b66820b
Urs Maurer <Urs@Maurer.net>
2023-12-29 15:31:11 +0100

    Simbad - added more id columns and Caldwell data

--- Python/Simbad Query.py
+++ Python/Simbad Query.py
@@ -2,7 +2,7 @@ from astroquery.simbad import Simbad
 import pandas as pd
 import sys
 
-def get_data(lower_magnitude, upper_magnitude):
+def get_data(lower_magnitude, upper_magnitude, upper_star_magnitude):
     """
     Query the Simbad database for astronomical objects within a specified magnitude range.
 
@@ -17,10 +17,10 @@ def get_data(lower_magnitude, upper_magnitude):
     customSimbad = Simbad()
     customSimbad.TIMEOUT = 1000  # Set the timeout for the query (in seconds)
     customSimbad.remove_votable_fields('coordinates')
-    customSimbad.add_votable_fields('id(name)', 'id(HD)', 'id(NGC)', 'ra(d)', 'dec(d)', 'pmra', 'pmdec', 'plx', 'otype', 'sp', 'flux(V)')
+    customSimbad.add_votable_fields('id(name)', 'id(HD)', 'id(HIP)', 'id(HR)', 'id(M)', 'id(NGC)', 'ra(d)', 'dec(d)', 'pmra', 'pmdec', 'plx', 'otype', 'sp', 'flux(V)')
 
     # Define the query to retrieve objects with a magnitude in the specified range
-    if upper_magnitude < 10.0:
+    if upper_magnitude < upper_star_magnitude:
         query = f"Vmag>={lower_magnitude} & Vmag<{upper_magnitude} & otype in ('G', 'ISM', 'Cl*', 'As*', '*')"
     else:
         query = f"Vmag>={lower_magnitude} & Vmag<{upper_magnitude} & otype in ('G', 'ISM', 'Cl*', 'As*')"
@@ -34,16 +34,31 @@ def get_data(lower_magnitude, upper_magnitude):
 
     return result_table
 
-# Get object tables within the specified magnitude range
-max_table_length = 20000
-lower_magnitude = -4.00
-all_dataframes = []
 
+"""
+   Get symbad objects in chunks of magnitude ranges
+   ================================================
+"""
+
+Min_Magnitude          = -2.0
+First_Upper_Magnitude  =  3.0
+Max_Star_Magnitude     = 10.0
+Max_Deap_Sky_Magnitude = 15.0
+
+max_table_length = 20000
+
+# Values in centi magnitudes
+first_upper_value = int(First_Upper_Magnitude * 100)
+last_upper_value  = int(Max_Deap_Sky_Magnitude * 100)
+increment         = 5
+
+all_dataframes  = []
+lower_magnitude = Min_Magnitude
 print("generate object table from magnitude", lower_magnitude)
-for deci_magnitude in range(500, 1500, 5):
-    upper_magnitude = float(deci_magnitude / 100.0)
+for centi_magnitude in range(first_upper_value, last_upper_value, increment):
+    upper_magnitude = float(centi_magnitude / 100.0)
     print(".. to magnitude", upper_magnitude)
-    object_table = get_data(lower_magnitude, upper_magnitude)
+    object_table = get_data(lower_magnitude, upper_magnitude, Max_Star_Magnitude)
     if object_table:
         dataframe = object_table.to_pandas()
         table_length = len(object_table)
@@ -62,7 +77,7 @@ if all_dataframes:
     result_dataframe = pd.concat(all_dataframes, ignore_index=True)
 
     # Select only the relevant columns
-    relevant_columns = ['MAIN_ID', 'ID_name', 'ID_HD', 'ID_NGC', 'RA_d', 'DEC_d', 'PMRA', 'PMDEC', 'PLX_VALUE', 'OTYPE', 'SP_TYPE', 'FLUX_V']
+    relevant_columns = ['MAIN_ID', 'ID_name', 'ID_HD', 'ID_HIP', 'ID_HR', 'ID_M', 'ID_NGC', 'RA_d', 'DEC_d', 'PMRA', 'PMDEC', 'PLX_VALUE', 'OTYPE', 'SP_TYPE', 'FLUX_V']
 
     # Save to CSV without index
     result_dataframe[relevant_columns].to_csv('simbad_data.csv', index=False)
