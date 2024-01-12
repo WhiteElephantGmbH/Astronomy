17a2b14eb06b83cfa6443f373208a8ccc471990b
Urs Maurer <Urs@Maurer.net>
2024-01-12 01:04:10 +0100

    Simbad Query

--- Python/Simbad Query.py
+++ Python/Simbad Query.py
@@ -1,29 +1,48 @@
+"""
+************************************************************************************************************************
+*                                         Query Data from the Simbad Database                                          *
+************************************************************************************************************************
+*                              (c) 2024 by White Elephant GmbH, Schaffhausen, Switzerland                              *
+*                                                www.white-elephant.ch                                                 *
+*                                                                                                                      *
+*      This program is free software; you can redistribute it and/or modify it under the terms of the GNU General      *
+*      Public License as published by the Free Software Foundation; either version 2 of the License, or                *
+*      (at your option) any later version.                                                                             *
+*                                                                                                                      *
+*      This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the      *
+*      implied warranty of MERCHANTABILITY or FITNESS for A PARTICULAR PURPOSE. See the GNU General Public License     *
+*      for more details.                                                                                               *
+*                                                                                                                      *
+*      You should have received a copy of the GNU General Public License along with this program; if not, write to     *
+*      the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                 *
+************************************************************************************************************************
+"""
 from astroquery.simbad import Simbad
 import pandas as pd
 import sys
 
-def get_data(lower_magnitude, upper_magnitude, upper_star_magnitude):
+def get_data(lower_ra, upper_ra):
     """
-    Query the Simbad database for astronomical objects within a specified magnitude range.
+    Query the Simbad database for deep sky objects within a specified ra range.
 
     Parameters:
-    - lower_magnitude (float): Lower limit of the magnitude range.
-    - upper_magnitude (float): Upper limit of the magnitude range.
+    - lower_ra: Lower limit of the right ascension in degrees.
+    - upper_ra: Upper limit of the right ascension in degrees.
 
     Returns:
     - result_table (astropy.table.Table): Table containing the query results.
     """
     # Set up the Simbad query
     customSimbad = Simbad()
-    customSimbad.TIMEOUT = 1000  # Set the timeout for the query (in seconds)
+    customSimbad.TIMEOUT = 1800 # Set the timeout 30 min for the query (in seconds)
     customSimbad.remove_votable_fields('coordinates')
-    customSimbad.add_votable_fields('id(name)', 'id(HD)', 'id(HIP)', 'id(HR)', 'id(M)', 'id(NGC)', 'ra(d)', 'dec(d)', 'pmra', 'pmdec', 'plx', 'otype', 'sp', 'flux(V)')
+    customSimbad.add_votable_fields('id(name)', 'id(HD)', 'id(HIP)', 'id(HR)', 'id(M)', 'id(NGC)', 'id(IC)', 'id(Ocl)',
+                                    'ra(d)', 'dec(d)', 'pmra', 'pmdec', 'plx', 'distance', 'otype(otypes)', 'sp',
+                                    'flux(R)', 'flux(G)', 'flux(B)', 'flux(V)')
 
-    # Define the query to retrieve objects with a magnitude in the specified range
-    if upper_magnitude < upper_star_magnitude:
-        query = f"Vmag>={lower_magnitude} & Vmag<{upper_magnitude} & otype in ('G', 'ISM', 'Cl*', 'As*', '*')"
-    else:
-        query = f"Vmag>={lower_magnitude} & Vmag<{upper_magnitude} & otype in ('G', 'ISM', 'Cl*', 'As*')"
+    # Define the query to retrieve deep sky objects
+    query = f"Ra>={lower_ra} & Ra<{upper_ra} & ((otype in ('G', 'ISM', 'Cl*', 'As*') & Vmag < 16.0)"\
+            " | (cat='HD' & Vmag < 12.0) | (cat='HIP' & Vmag < 12.0) | cat='HR' | cat='M')"
 
     # Execute the query
     try:
@@ -36,29 +55,21 @@ def get_data(lower_magnitude, upper_magnitude, upper_star_magnitude):
 
 
 """
-   Get symbad objects in chunks of magnitude ranges
-   ================================================
+   Get symbad objects in chunks of Ra ranges
+   =========================================
 """
 
-Min_Magnitude          = -2.0
-First_Upper_Magnitude  =  3.0
-Max_Star_Magnitude     = 10.0
-Max_Deap_Sky_Magnitude = 15.0
+Output_File = "data.csv"
 
 max_table_length = 20000
 
-# Values in centi magnitudes
-first_upper_value = int(First_Upper_Magnitude * 100)
-last_upper_value  = int(Max_Deap_Sky_Magnitude * 100)
-increment         = 5
-
 all_dataframes  = []
-lower_magnitude = Min_Magnitude
-print("generate object table from magnitude", lower_magnitude)
-for centi_magnitude in range(first_upper_value, last_upper_value, increment):
-    upper_magnitude = float(centi_magnitude / 100.0)
-    print(".. to magnitude", upper_magnitude)
-    object_table = get_data(lower_magnitude, upper_magnitude, Max_Star_Magnitude)
+
+for lower_ra in range(0, 3600, 1):
+    from_ra = lower_ra / 10.0
+    to_ra = (lower_ra + 1) / 10.0
+    print("from RA:", from_ra, "to RA:", to_ra)
+    object_table = get_data(from_ra, to_ra)
     if object_table:
         dataframe = object_table.to_pandas()
         table_length = len(object_table)
@@ -69,7 +80,6 @@ for centi_magnitude in range(first_upper_value, last_upper_value, increment):
     else:
         print ("object table empty")
         sys.exit()
-    lower_magnitude = upper_magnitude
 
 # Process data
 if all_dataframes:
@@ -77,10 +87,12 @@ if all_dataframes:
     result_dataframe = pd.concat(all_dataframes, ignore_index=True)
 
     # Select only the relevant columns
-    relevant_columns = ['MAIN_ID', 'ID_name', 'ID_HD', 'ID_HIP', 'ID_HR', 'ID_M', 'ID_NGC', 'RA_d', 'DEC_d', 'PMRA', 'PMDEC', 'PLX_VALUE', 'OTYPE', 'SP_TYPE', 'FLUX_V']
+    relevant_columns = ['MAIN_ID', 'ID_name', 'ID_HD', 'ID_HIP', 'ID_HR', 'ID_M', 'ID_NGC', 'ID_IC', 'ID_Ocl',
+                        'RA_d', 'DEC_d', 'PMRA', 'PMDEC', 'PLX_VALUE', 'Distance_distance', 'Distance_unit',
+                        'OTYPE_otypes', 'SP_TYPE', 'FLUX_R', 'FLUX_G', 'FLUX_B', 'FLUX_V']
 
     # Save to CSV without index
-    result_dataframe[relevant_columns].to_csv('simbad_data.csv', index=False)
-    print("Data saved to 'simbad_data.csv'")
+    result_dataframe[relevant_columns].to_csv(Output_File, index=True)
+    print("Data saved to " + Output_File)
 else:
     print("No objects found.")
