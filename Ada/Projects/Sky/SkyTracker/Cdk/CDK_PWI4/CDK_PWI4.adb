f7edc0b729f9deba0f1ad0118fe42533e2361a28
Urs <Urs@Maurer.net>
2024-03-15 10:33:09 +0100

    CDK_PWI4 v4.0/0-3 - use latest text handling.

--- Ada/Projects/Sky/SkyTracker/Cdk/CDK_PWI4/CDK_PWI4.adb
+++ Ada/Projects/Sky/SkyTracker/Cdk/CDK_PWI4/CDK_PWI4.adb
@@ -16,7 +16,7 @@
 pragma Style_White_Elephant;
 
 pragma Build (Description => "SkyTracker control program for CDK700 (PWI4)",
-              Version     => (4, 0, 0, 2),
+              Version     => (4, 0, 0, 3),
               Kind        => Windows,
               Libraries   => ("AWS64", "COLL64"),
               Compiler    => "GNATPRO\23.0");
