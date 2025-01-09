8e6e835665e130c908b74fbfadb2d2d8ea79a7f1
Urs <Urs@Maurer.net>
2025-01-09 23:52:14 +0100

    SkyTracker Release PWI4 v4.0.8.0, GM v2.4.5.0 - improved TCP/IP packages.

--- Ada/Projects/Sky/SkyTracker/HPS/GM_1000/SkyTracker/SkyTracker.adb
+++ Ada/Projects/Sky/SkyTracker/HPS/GM_1000/SkyTracker/SkyTracker.adb
@@ -1,5 +1,5 @@
 -- *********************************************************************************************************************
--- *                       (c) 2022 .. 2024 by White Elephant GmbH, Schaffhausen, Switzerland                          *
+-- *                       (c) 2022 .. 2025 by White Elephant GmbH, Schaffhausen, Switzerland                          *
 -- *                                               www.white-elephant.ch                                               *
 -- *                                                                                                                   *
 -- *    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General     *
@@ -16,7 +16,7 @@
 pragma Style_White_Elephant;
 
 pragma Build (Description => "SkyTracker control program for GM 1000 HPS",
-              Version     => (2, 4, 4, 0),
+              Version     => (2, 4, 5, 0),
               Kind        => Windows,
               Libraries   => ("AWSS64", "COLL64"),
               Compiler    => "GNATPRO\23.0");
