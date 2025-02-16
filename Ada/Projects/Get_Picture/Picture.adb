b33fd4249f40a2c6a9a4e95d878f0aab5eaeb4ee
Urs Maurer <Urs@Maurer.net>
2025-02-16 15:18:58 +0100

    Get_Picture v1.3/0-0 - Allow waiting for a picture.

--- Ada/Projects/Get_Picture/Picture.adb
+++ Ada/Projects/Get_Picture/Picture.adb
@@ -5,9 +5,12 @@
 pragma Style_White_Elephant;
 
 with Ada.Command_Line;
+with Ada.IO_Exceptions;
+with Ada.Text_IO;
 with Application;
 with Configuration;
 with Directory;
+with Exceptions;
 with File;
 with Gui;
 with Log;
@@ -15,9 +18,11 @@ with Text;
 
 package body Picture is
 
-  subtype Check_Duration is Duration range 0.01 .. 2.0;
+  type Detection_Timeout is delta 0.1 range 0.0 .. 20.0;
 
-  Default_Check_Delay : constant Check_Duration := 0.02; -- default 200 ms
+  Detection_Delay : constant := 1.0;
+
+  Default_Detection_Timeout : constant Detection_Timeout := 3.0;
 
   Default_Dirctory : constant String := "D:\SharpCap\";
 
@@ -69,74 +74,83 @@ package body Picture is
 
   procedure Copy_Completed (The_Filename     : String;
                             Destination      : String;
-                            Size_Check_Delay : Check_Duration) is
+                            Size_Check_Delay : Duration := 0.2) is
 
     Target_Folder      : constant String := File.Containing_Directory_Of (Destination);
     Temporary_Filename : constant String := File.Composure (Target_Folder, File.Base_Name_Of (The_Filename), "tmp");
 
-    The_Filesize : File.Size;
     New_Filesize : File.Size;
     use type File.Size;
 
-    Delay_Time  : constant Duration := 0.1;
-    The_Timeout : Duration := 5.0;
+    procedure Await_Stable (The_Filename : String) is
+      The_Filesize : File.Size := File.Size_Of (The_Filename);
+      The_File : Ada.Text_IO.File_Type;
+    begin
+      Log.Write ("Await access to " & The_Filename);
+      loop
+        begin
+          Ada.Text_IO.Open (The_File, Ada.Text_IO.Append_File, The_Filename);
+          exit;
+        exception
+        when Ada.Text_IO.Use_Error =>
+          delay 0.5;
+        end;
+      end loop;
+      Ada.Text_IO.Close (The_File);
+      Log.Write ("Await stable filesize for " & The_Filename);
+      loop
+        delay Size_Check_Delay;
+        New_Filesize := File.Size_Of (The_Filename);
+        Log.Write ("New file size:" & New_Filesize'image);
+        if New_Filesize = 0 then
+          Error ("File " & The_Filename & " is empty");
+        end if;
+        exit when New_Filesize = The_Filesize;
+        The_Filesize := New_Filesize;
+        delay 0.2;
+      end loop;
+      Log.Write ("Filesize is stable");
+    end Await_Stable;
 
   begin
     if not Directory.Exists (Target_Folder) then
       Error ("Destination directory " & Target_Folder & " not found");
     end if;
-    The_Filesize := File.Size_Of (The_Filename);
-    loop
-      delay Size_Check_Delay;
-      New_Filesize := File.Size_Of (The_Filename);
-      if New_Filesize = 0 then
-        Error ("File " & The_Filename & " is empty");
-      end if;
-      exit when New_Filesize = The_Filesize;
-      The_Filesize := New_Filesize;
-    end loop;
-    loop
-      begin
-        File.Copy (The_Filename, Temporary_Filename);
-        exit;
-      exception
-      when File.Use_Error =>
-        The_Timeout := The_Timeout - Delay_Time;
-        if The_Timeout <= 0.0 then
-          Error ("Cant't write file " & Temporary_Filename);
-        end if;
-        delay Delay_Time;
-      end;
-    end loop;
-    begin
-      File.Rename (Temporary_Filename, Destination);
-      File.Delete (Temporary_Filename);
-    exception
-    when File.Use_Error =>
-      Error ("Can't write file " & Destination);
-    end;
+    Await_Stable (The_Filename);
+    Log.Write ("Copy to temporary file");
+    File.Copy (The_Filename, Temporary_Filename);
+    Await_Stable (Temporary_Filename);
+    Log.Write ("Rename temporary file");
+    File.Rename (Temporary_Filename, Destination);
+    Log.Write ("Delete temporary file");
+    File.Delete (Temporary_Filename);
+    Log.Write ("Copy complete");
+  exception
+  when File.Use_Error =>
+    Error ("Can't write file " & Destination);
   end Copy_Completed;
 
 
   package Cmd  renames Ada.Command_Line;
 
   procedure Get is
-    Argument_Count   : constant Natural := Cmd.Argument_Count;
-    Size_Check_Delay : Check_Duration := Default_Check_Delay;
+    Argument_Count         : constant Natural := Cmd.Argument_Count;
+    File_Detection_Timeout : Detection_Timeout := Default_Detection_Timeout;
   begin
     if Argument_Count > 2 then
-      Error ("Parameters syntax: [<source directory name> [<size check delay>]");
+      Error ("Parameters syntax: [<source directory name> [<file detection timeout>]");
     elsif Argument_Count = 2 then
       begin
-        Size_Check_Delay := Check_Duration'value(Cmd.Argument(2));
+        File_Detection_Timeout := Detection_Timeout'value(Cmd.Argument(2));
       exception
       when others =>
-        Error ("Size check delay not in range 0.01 .. 2.0");
+        Error ("Size check delay not in range" & Detection_Timeout'first'image & " .." &  Detection_Timeout'last'image);
       end;
     end if;
     declare
-      Picture_Directory : constant String := (if Argument_Count = 0 then Default_Dirctory else Cmd.Argument(1));
-      The_Filename      : Text.String;
+      Picture_Directory  : constant String := (if Argument_Count = 0 then Default_Dirctory else Cmd.Argument(1));
+      The_Filename       : Text.String;
+      The_Detection_Time : Duration := 0.01; -- must be > 0.0
       use type Text.String;
     begin
       if not Directory.Exists (Picture_Directory) then
@@ -145,19 +159,27 @@ package body Picture is
       if File.Exists (Destination_Filename) then
         Error ("Destionation filename " & Destination_Filename & " already exists");
       end if;
-      for Filename of File.Iterator_For (Picture_Directory) loop
-        if Text.Lowercase_Of (File.Extension_Of (Filename)) in "cr2" | "fits" | "jpeg" | "jpg" then
-          if The_Filename.Is_Empty or else File.Is_Newer (Filename, +The_Filename) then
-            The_Filename := [Filename];
+      loop
+        for Filename of File.Iterator_For (Picture_Directory) loop
+          if Text.Lowercase_Of (File.Extension_Of (Filename)) in "cr2" | "fits" | "jpeg" | "jpg" then
+            if The_Filename.Is_Empty or else File.Is_Newer (Filename, +The_Filename) then
+              The_Filename := [Filename];
+            end if;
           end if;
+        end loop;
+        exit when not The_Filename.Is_Empty;
+        if The_Detection_Time > Duration(File_Detection_Timeout) then
+          Error ("No picture file found in " & Picture_Directory);
         end if;
+        The_Detection_Time := @ + Detection_Delay;
+        delay Detection_Delay;
       end loop;
-      if The_Filename.Is_Empty then
-        Error ("No picture file in source directory");
-      end if;
-      Copy_Completed (+The_Filename, Destination_Filename, Size_Check_Delay);
+      Copy_Completed (+The_Filename, Destination_Filename);
     end;
   exception
+  when Item: Ada.IO_Exceptions.Use_Error =>
+    Log.Write (Item);
+    Gui.Message_Box (Exceptions.Name_Of (Item));
   when Error_Detected =>
     null;
   when Occurrence: others =>
